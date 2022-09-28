eb893069a8b80de19d510c7569471f0b0f35bbe9
oessayeg <offissayegh001@gmail.com>
2022-09-28 18:14:00 +0100

    Added max_size function

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ int main( void )
     std::vector<int> test;
 	ft::vector<int> test1;
 
-    // test.push_back(1);
-    // test.push_back(2);
-    // test.push_back(3);
-    // test.push_back(4);
-    // test.push_back(5);
+	test.push_back(1);
+	test.push_back(2);
+	test.push_back(3);
+	test.push_back(4);
 
-    std::cout << test.max_size() << std::endl;
-    std::cout << test1.max_size() << std::endl;
+	test.clear();
+
+	std::cout << "Size = " << test.size() << std::endl;
+	std::cout << "Capacity = " << test.capacity() << std::endl;
+
     return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -3,73 +3,51 @@
 
 namespace ft{
 
+//Still need copy constructor and copy assignement operator overload
+
 template<class T, class Allocator = std::allocator<T> >
 class vector
 {
 	private :
+		typedef unsigned long size_type;
 		T *arr;
-		unsigned int sizeOfVec;
-		unsigned int capacityOfVec;
+		Allocator allocator;
+		size_type sizeOfVec;
+		size_type capacityOfVec;
 	public :
-		vector( void );
-		// vector( vector const &rhs );
-		~vector( void );
-		// vector &operator=( vector const &rhs );
-		T &operator[]( unsigned int idx );
-		T &at( unsigned int idx );
-		unsigned int size( void ) const;
-		unsigned int capacity( void ) const;
-		//test
-		unsigned int max_size() const
+		//Default contructor
+		vector( void ) : arr(NULL), sizeOfVec(0), capacityOfVec(0) {}
+
+		//Destructor
+		~vector( void )
 		{
-			return Allocator::max_size();
+			if (arr)
+				delete arr;
 		}
-		//test
+		
+		//Subscript operator
+		T &operator[]( size_type idx ) { return arr[idx]; }
+
+		//At member function
+		T &at( size_type idx )
+		{
+			if (idx > sizeOfVec || (idx == 0 && sizeOfVec == 0))
+				throw std::out_of_range("vector");
+			return arr[idx];
+		}
+
+		//Size member function
+		size_type size( void ) const { return sizeOfVec; }
+
+		//Capacity member function
+		size_type capacity( void ) const { return capacityOfVec; }
+
+		//Max size to allocate
+		size_type max_size( void ) const { return allocator.max_size(); }
+
+		//Front member function
+		T &front( void ) { return arr[0]; }
 };
-
-//Default constructor
-template< class T, class Allocator >
-vector< T, Allocator >::vector( void ) : arr(NULL), sizeOfVec(0), capacityOfVec(0){}
-
-//Destructor
-template< class T, class Allocator >
-vector< T, Allocator >::~vector( void )
-{
-	if (arr)
-		delete arr;
-}
-
-//Subscript operator
-template< class T, class Allocator >
-T &vector< T, Allocator >::operator[]( unsigned int idx )
-{
-	return arr[idx];
-}
-
-//At
-template< class T, class Allocator >
-T &vector< T, Allocator >::at( unsigned int idx )
-{
-	if (idx > sizeOfVec || (idx == 0 && sizeOfVec == 0))
-		throw std::out_of_range("vector");
-	return arr[idx];
-}
-
-//Size
-template< class T, class Allocator >
-unsigned int vector< T, Allocator >::size( void ) const
-{
-	return sizeOfVec;
-}
-
-//Capacity
-template< class T, class Allocator >
-unsigned int vector< T, Allocator >::capacity( void ) const
-{
-	return capacityOfVec;
-}
-
-
 
 
 }
