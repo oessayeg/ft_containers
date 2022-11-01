be8350093fdffaab4c59c3c85ed93d0824cacac1
oessayeg <offissayegh001@gmail.com>
2022-11-01 16:16:49 +0100

    Added iterDistance function (between 2 iter):
    It calculates the distance between 2 iterators :
    returns iter2 - iter1 for random access iterators
    iterator from iter1 to iter2 and returns distance for inputIterators

--- main.cpp
+++ main.cpp
@@ -10,65 +10,39 @@
 //fixed today : assign range
 //to fix : erase, erase_mixed, erase_range, insert_mixed, insert_range
 
-template < class iter >
-void checkIter( iter iterator )
+template < class vector >
+void checkIter( vector vect )
 {
-	if (typeid(typename ft::iterator_traits< iter >::iterator_category) == typeid(std::random_access_iterator_tag))
-		std::cout << "Random access" << std::endl;
-	else if (typeid(typename ft::iterator_traits< iter >::iterator_category) == typeid(std::input_iterator_tag))
-		std::cout << "Input iterator" << std::endl;
-	std::cout << *iterator << std::endl;
+	for (typename vector::iterator b = vect.begin(); b != vect.end(); b++)
+		std::cout << *b << " ";
+	std::cout << std::endl;
 }
+
+template < class T, class U >
+struct is_same
+{
+	static const bool value = false;
+};
+
+template<class T>
+struct is_same<T, T>
+{
+	static const bool value = true;
+};
 
 int main( void )
 {
 	{
-        // std::vector<char> v(2, 'U');
-		// std::istringstream str("1 2 3 4 5 6 7 8 9 0 A B C D E F");
-        // std::istreambuf_iterator<char> it(str), end;
-        // // std::istreambuf_iterator<char> itCopy = it, endCopy = end;
+	    std::istringstream str("1 2 3 4 5 6 7 8 9 0 A B C D E F");
+        std::istreambuf_iterator<char> it(str), end;
+		std::vector<char> v;
 
-		// checkIter(it);
-		// // for (; itCopy != endCopy; itCopy++)
-		// // 	std::cout << *itCopy << " ";
-		
-		// // std::cout << std::endl;
-
-		// // for (; it != end; it++)
-		// // 	std::cout << *it << " ";
-
-		// std::cout << std::endl;
-
-
-	    // v.insert(v.end(), it, end);
-	
-		// std::vector<char> v(20);
-        
-        // std::istringstream str("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22");
-        // std::istreambuf_iterator<char> it(str), end;
-
-		// v.assign(it, end);
-		// std::cout << "Capacity = " << v.capacity() << std::endl;
-		// std::cout << "Size = " << v.size() << std::endl;
-		// print(v);
+		std::cout << (is_same<std::input_iterator_tag, ft::iterator_traits< std::istreambuf_iterator<char> >::iterator_category >::value) << std::endl;
 	}
 
 	std::cout << "---------FT---------" << std::endl;
 
 	{
-        ft::vector v(2, 'U');
-		std::istringstream str("1 2 3 4 5 6 7 8 9 0 A B C D E F");
-        std::istreambuf_iterator<char> it(str), end;
-		
-		ft::iterDistance(it, end);
 
-        // v.insert(v.end(), it, end);
-		// ft::vector<char> v;
-        
-        // std::istringstream str("1 2 3 4 5 6 7");
-        // std::istreambuf_iterator<char> it(str), end;
-
-		// v.assign(it, end);
-		// print(v);
 	}
 }
--- utils.hpp
+++ utils.hpp
@@ -42,14 +42,38 @@ namespace ft
         typedef const T& reference;
         typedef std::random_access_iterator_tag iterator_category;
     };
+    
+    template < class iterator >
+    typename iterator_traits< iterator >::difference_type Distance( iterator a, iterator b, std::random_access_iterator_tag )
+    {
+        return b - a;
+    }
+
+    template < class iterator >
+    typename iterator_traits< iterator >::difference_type Distance( iterator a, iterator b, std::input_iterator_tag )
+    {
+        typename iterator_traits< iterator >::difference_type dist = 0;
+        
+        for (; a != b; a++)
+            dist++;
+        return dist;
+    }
 
     template < class iterator >
     typename iterator_traits< iterator >::difference_type iterDistance( iterator a, iterator b )
     {
-        typename iterator_traits< iterator >::difference_type distance = 0;
-
-        for (; a != b; a++)
-            distance++;
-        return distance;
+        return Distance(a, b, typename ft::iterator_traits< iterator >::iterator_category());
     }
+
+    template < class T, class U >
+    struct is_same
+    {
+	    static const bool value = false;
+    };
+
+    template<class T>
+    struct is_same<T, T>
+    {
+	    static const bool value = true;
+    };
 }
